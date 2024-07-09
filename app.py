b4aa8d6a0bf3bc6db011e746df6a8c4a2590648e
Seline-D <selinedata@gmail.com>
2024-07-10 01:07:51 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+# Import the necessary packages
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
